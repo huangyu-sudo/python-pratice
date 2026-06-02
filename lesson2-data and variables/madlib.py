543bee8dfe1796de4a781da09747dec28fbca93d
huangyu-sudo <huangyu@wsc.school.nz>
2026-06-02 14:15:37 +1200

    Create madlib.py

---
+++ lesson2-data and variables/madlib.py
@@ -0,0 +1,24 @@
+# Create a short Madlib: Get input from your user (a bunch of words), 
+# then output a madlib using those words.
+
+# Ask user for a name and save it in a variable
+name = input("what is you name")
+
+# Ask user for an animal and save it in a variable
+animal=input("what is your favorite animal")
+# Ask user for a colour and save it in a variable
+colour=input("what is your favorite colour")
+# Print your madlib using the 3 variables above.
+print(name+animal+colour)
+# ----------------------------
+
+# EXTENSION
+# Research about 'print formatting in python'. 
+# Use what you learn to rewrite your madlib into easier to read code.
+
+# ----------------------------
+
+# EXPERT (for those who already know some Python)
+# Create a randomised madlib game
+# GOAL: Just like above except...
+#       Write 4-6 different madlibs and randomise which one is output.
