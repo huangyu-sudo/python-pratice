2c1a8481a338ffb5b39e0a50351941372ee60228
huangyu-sudo <huangyu@wsc.school.nz>
2026-06-02 14:15:26 +1200

    Create quiz.py

---
+++ quiz.py
@@ -0,0 +1,12 @@
+q1 = input("Do you love sunny days?")
+if q1 == "yes":
+    print("Sunshine makes people happy!")
+elif q1 == "i don't know":
+    print("rain is ok too")
+else:
+    print("Cloudy days are also comfortable.")
+q1 = input("Do you like reading books? ").lower()
+if q1 == "yes":
+    print("Nice hobby!")
+else:
+    print("You can try it sometime.“）
